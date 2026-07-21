c6d5dcd300f91fc8bc253244e8e95578a97770c1
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-22 06:55:48 +0800

    sring includes, endsWith, stardsWith

---
+++ includs.js
@@ -0,0 +1,14 @@
+const emil="moazzem@.come";
+console.log(emil.includes(".com"));
+
+const book="rahimen";
+console.log(emil.includes("M"));
+
+
+
+const father="manfarhe@.come";
+console.log(father.endsWith("come"));
+
+
+const mother='heooll.png';
+console.log(mother.endsWith("png"));
