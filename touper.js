f1afa22e6c8376c43c3fe475be2f5c880ae617b2
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-22 06:44:38 +0800

    first commit

---
+++ touper.js
@@ -0,0 +1,10 @@
+const book="ENGLISH";
+console.log(book.toLocaleLowerCase());
+// const name="moazzem ";
+// console.log(name.trime());
+
+
+
+
+const hello="moazzem ";
+console.log(hello.trim().toUpperCase(),"2222")
