29381a2a3bb8cfc7035dac3c1f68b811f7a91dac
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-22 08:22:50 +0800

    keys value dot notation and bracket notatino

---
+++ keys.js
@@ -0,0 +1,15 @@
+const person ={
+    name:"akbuer ali",
+    age:54,
+    isMaride:true,
+}
+console.log(person);
+console.log(person.name);
+const keys=Object.keys(person);
+console.log(keys);
+
+const value=Object.values(person);
+console.log(value);
+
+delete person(isMaride);
+console.log(peson)
